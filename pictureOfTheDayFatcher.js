aca8a495e2fd7ef9565476a20c941ced0b03425c
entropy88 <36485631+entropy88@users.noreply.github.com>
2020-03-22 13:41:00 +0100

    fixed video display of star images, added scroll to articles

--- pictureOfTheDayFatcher.js
+++ pictureOfTheDayFatcher.js
@@ -7,21 +7,37 @@ function getPictureOfTheDay(){
     .then(function(resp) {
         console.log(resp.url);
         console.log(imagContainer);
+      
         let title=document.createElement("p");
         title.innerText=resp.title;
         title.className="titleP";
         imagContainer.appendChild(title);
-        let imgOfTheDay=document.createElement("img");
-        imgOfTheDay.setAttribute("src",resp.url);
-        imgOfTheDay.className="starImage";
-        imagContainer.appendChild(imgOfTheDay);
+
         let description=document.createElement("p");
         description.innerText=resp.explanation;
         description.className="descriptionP";
         imagContainer.appendChild(description);
+
+        if(resp.media_type=="video"){
+          console.log("it's a video")
+          let iframeVid=document.createElement("iframe");
+          iframeVid.setAttribute("src", resp.url);
+          iframeVid.className="starVideo"
+          imagContainer.appendChild(iframeVid);
+
+        } else {
+        let imgOfTheDay=document.createElement("img");
+        imgOfTheDay.setAttribute("src",resp.url);
+        imgOfTheDay.className="starImage";
+        imagContainer.appendChild(imgOfTheDay);
+        }
+
+    
  
 })
 .catch(function() {
   alert("Нещо се обърка...")
 });
     }
+
+ 
